9be8f03e7e2fa4762962f1e9fa763fd20c37005c
ormosh <ormoshg@gmail.com>
2025-06-27 12:10:38 +0200

    urls name/template extends/snippet model + migrations

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -5,6 +5,6 @@ from MainApp import views
 
 urlpatterns = [
     path('', views.index_page),
-    path('snippets/add', views.add_snippet_page),
-    path('snippets/list', views.snippets_page),
+    path('snippets/add', views.add_snippet_page, name='add-snippet-page'),
+    path('snippets/list', views.snippets_page, name='snippets-page'),
 ]
